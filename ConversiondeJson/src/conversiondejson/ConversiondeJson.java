9dcade0177b97ab418b059517797c16061c6cb77
Eliasim <Eliasim@DESKTOP-SELG81J>
2020-04-24 14:20:37 -0500

    Librerias Externas

---
+++ ConversiondeJson/src/conversiondejson/ConversiondeJson.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conversiondejson;
+
+/**
+ *
+ * @author Eliasim
+ */
+public class ConversiondeJson {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FRMPrincicpal x = new FRMPrincicpal();
+        x.setVisible(true);
+    }
+    
+}
